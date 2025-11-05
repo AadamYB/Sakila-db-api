62d627cef9185f90db07746524930704f386e186
Aadam <ec221024@qmul.ac.uk>
2025-11-05 12:52:25 +0000

    Check for films and actors and also implemented pagination for actors

--- api/routes/actor.py
+++ api/routes/actor.py
@@ -78,6 +78,13 @@ def delete_actor(actor_id):
 @actors_router.get('/<int:actor_id>/films')
 def list_actor_films(actor_id):
     actor = Actor.query.get(actor_id)
+
+    if not actor:
+        return 'Actor Not Found', 404
+    
+    if not actor.films:
+        return 'No Films Found for this Actor', 404
+    
     return jsonify([{
         'film_id': f.film_id, 
         'title': f.title,
@@ -86,3 +93,37 @@ def list_actor_films(actor_id):
             }
         } 
         for f in actor.films]), 200
+
+
+@actors_router.get('')
+def list_actors_per_page():
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per-page', 10, type=int)
+    f_name_filter = request.args.get('first_name', None, type=str)
+    l_name_filter = request.args.get('last_name', None, type=str)
+ 
+    query = Actor.query
+
+    if f_name_filter:
+        query = query.filter(Actor.first_name.ilike(f'%{f_name_filter}%'))
+    if l_name_filter:
+        query = query.filter(Actor.last_name.ilike(f'%{l_name_filter}%'))
+
+    actor_pagination = query.paginate(page=page, per_page=per_page)
+    actors = actor_pagination.items
+
+    
+    return jsonify({
+        'results': actors_schema.dump(actors),
+        'pagination': {
+            'count': actor_pagination.total,
+            'page': actor_pagination.page,
+            'per_page': actor_pagination.per_page,
+            'pages': actor_pagination.pages,
+            'links': {
+                'self_url': url_for('api.actors.list_actors_per_page', page=actor_pagination.page, per_page=per_page, _external=True),
+                'next_url': url_for('api.actors.list_actors_per_page', page=actor_pagination.next_num, per_page=per_page, _external=True) if actor_pagination.has_next else None,
+                'prev_url': url_for('api.actors.list_actors_per_page', page=actor_pagination.prev_num, per_page=per_page, _external=True) if actor_pagination.has_prev else None,
+            }
+        }
+    }), 200
